6143c952ff75a6c1bebe5ac6c33cdae8cf77f7d2
Luke Halley <luke123halley@gmail.com>
2021-05-06 17:23:24 +0100

    adding error handling

--- src/store/modules/beats/actions.js
+++ src/store/modules/beats/actions.js
@@ -2,11 +2,25 @@ import { API } from 'aws-amplify';
 import { createBeat } from '@/graphql/mutations';
 import * as queries from '@/graphql/queries';
 
+function cleanTags(tagObject) {
+    return Object.entries(tagObject).reduce((a, [k, v]) => (v == null ? a : (a[k] = v, a)), {})
+}
+
 export default {
     async uploadBeat(context, formData) {
         // Get current user id and username.
         const userId = context.rootGetters["userStore/userId"]
         const username = context.rootGetters["userStore/username"]
+
+        // Get the beats tags.
+        var tags = {
+            tag1: formData.tags[0],
+            tag2: formData.tags[1],
+            tag3: formData.tags[2]
+        }
+
+        // Remove any null tags.
+        let cleanedTags = cleanTags(tags)
 
         // Create new Beat object.
         const newBeat = {
@@ -42,11 +56,7 @@ export default {
                 genre2: "Hip Hop",
                 genre3: "South"
             },
-            tags: {
-                tag1: formData.tags[0],
-                tag2: formData.tags[1],
-                tag3: formData.tags[2]
-            },
+            tags: cleanedTags,
             public: true,
             price: {
                 MP3Price: formData.prices.mp3Price,
@@ -86,7 +96,12 @@ export default {
 
         // Create the beat, catch any errors.
         try {
-            await promise;
+            await promise.then(function (result) {
+                console.log(result);
+
+                // Add the newely created beat to our local list of Beats.
+                context.commit('addBeatLocally', { ...newBeat, id: userId })
+            });
         } catch (error) {
             // Print out the actual error given back to us.
             console.error(error.errors[0].message);
@@ -97,83 +112,81 @@ export default {
                 console.error(error.message);
             }
         }
-
-        // Add the newely created beat to our local list of Beats.
-        context.commit('addBeatLocally', { ...newBeat, id: userId })
     },
     async getBeats(context) {
+        // Fetch the beats.
+        const fetchBeats = API.graphql({ query: queries.listBeats })
 
         // Execute the get request, catch any errors.
         try {
-            // Fetch the beats.
-            const fetchBeats = await API.graphql({ query: queries.listBeats });
+            await (fetchBeats).then(function (beats) {
+                // Gets the list of beats.
+                const recievedBeats = beats.data.listBeats.items;
 
-            // Gets the list of beats.
-            const recievedBeats = fetchBeats.data.listBeats.items;
+                // Create new beats list.
+                const newBeats = []
 
-            // Create new beats list.
-            const newBeats = []
+                for (const key in recievedBeats) {
 
-            for (const key in recievedBeats) {
+                    // Get the beats tags.
+                    var tags = {
+                        tag1: recievedBeats[key].tags.tag1,
+                        tag2: recievedBeats[key].tags.tag2,
+                        tag3: recievedBeats[key].tags.tag3
+                    }
 
-                // Get the beats tags.
-                var tags = {
-                    tag1: recievedBeats[key].tags.tag1,
-                    tag2: recievedBeats[key].tags.tag2,
-                    tag3: recievedBeats[key].tags.tag3
+                    // Remove any null tags.
+                    let cleanedTags = cleanTags(tags)
+
+                    const beat = {
+                        id: recievedBeats[key].id,
+                        title: recievedBeats[key].title,
+                        owner: {
+                            ownerId: recievedBeats[key].owner.ownerId,
+                            ownerUsername: recievedBeats[key].owner.ownerUsername
+                        },
+                        thumbnail: {
+                            region: recievedBeats[key].thumbnail.region,
+                            bucket: recievedBeats[key].thumbnail.bucket,
+                            key: recievedBeats[key].thumbnail.key
+                        },
+                        genre: {
+                            genre1: recievedBeats[key].genre.genre1,
+                            genre2: recievedBeats[key].genre.genre2,
+                            genre3: recievedBeats[key].genre.genre3
+                        },
+                        tags: cleanedTags,
+                        public: true,
+                        price: {
+                            MP3Price: recievedBeats[key].price.MP3Price,
+                            WAVPrice: recievedBeats[key].price.WAVPrice,
+                            TrackoutPrice: recievedBeats[key].price.TrackoutPrice,
+                            UnlimitedPrice: recievedBeats[key].price.UnlimitedPrice,
+                            ExclusivePrice: recievedBeats[key].price.ExclusivePrice
+                        },
+                        bpm: recievedBeats[key].bpm,
+                        schedule: recievedBeats[key].schedule,
+                        free: recievedBeats[key].free,
+                        createdAt: recievedBeats[key].createdAt,
+                        updatedAt: recievedBeats[key].updatedAt
+                    }
+
+                    newBeats.push(beat)
+
                 }
 
-                // Remove any null tags.
-                let cleanedTags = Object.entries(tags).reduce((a, [k, v]) => (v == null ? a : (a[k] = v, a)), {})
-
-                const beat = {
-                    id: recievedBeats[key].id,
-                    title: recievedBeats[key].title,
-                    owner: {
-                        ownerId: recievedBeats[key].owner.ownerId,
-                        ownerUsername: recievedBeats[key].owner.ownerUsername
-                    },
-                    thumbnail: {
-                        region: recievedBeats[key].thumbnail.region,
-                        bucket: recievedBeats[key].thumbnail.bucket,
-                        key: recievedBeats[key].thumbnail.key
-                    },
-                    genre: {
-                        genre1: recievedBeats[key].genre.genre1,
-                        genre2: recievedBeats[key].genre.genre2,
-                        genre3: recievedBeats[key].genre.genre3
-                    },
-                    tags: cleanedTags,
-                    public: true,
-                    price: {
-                        MP3Price: recievedBeats[key].price.MP3Price,
-                        WAVPrice: recievedBeats[key].price.WAVPrice,
-                        TrackoutPrice: recievedBeats[key].price.TrackoutPrice,
-                        UnlimitedPrice: recievedBeats[key].price.UnlimitedPrice,
-                        ExclusivePrice: recievedBeats[key].price.ExclusivePrice
-                    },
-                    bpm: recievedBeats[key].bpm,
-                    schedule: recievedBeats[key].schedule,
-                    free: recievedBeats[key].free,
-                    createdAt: recievedBeats[key].createdAt,
-                    updatedAt: recievedBeats[key].updatedAt
-                }
-
-                newBeats.push(beat)
-            }
-
-            // Add the fetched beats to our local list of Beats.
-            context.commit('setBeatsLocally', newBeats)
+                // Add the fetched beats to our local list of Beats.
+                context.commit('setBeatsLocally', newBeats)
+            });
         } catch (error) {
-            // Print out the actual error given back to us.
-            console.error(error);
+            // If getting our beats caused an error, throw it.
+            const errorToThrow = new Error(error.errors[0].message || "Failed to load beats!")
 
-            // If the error is because the request was cancelled we can confirm here.
-            if (API.isCancel(error)) {
-                // handle user cancellation logic.
-                console.error(error.message);
-            }
+            // When an error is thrown, the component which dispatched the action it can handle it.
+            throw errorToThrow
         }
+
+
 
     }
 };
