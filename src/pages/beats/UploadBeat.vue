48e3c3e2a2177d25437c09856205991bc147024e
Luke Halley <luke123halley@gmail.com>
2021-07-08 19:28:15 +0800

    edit beat working

--- src/pages/beats/UploadBeat.vue
+++ src/pages/beats/UploadBeat.vue
@@ -5,11 +5,11 @@
         <h2>Upload A Beat</h2>
         <beat-upload
             @upload-beat="uploadBeat"
-            :beatTitle="{value: null, valid: true}"
-            :beatBPM = "{value: null, valid: true}"
-            :beatMp3Price = "{value: null, valid: true}"
-            :beatWavPrice = "{value: null, valid: true}"
-            :beatZipPrice = "{value: null, valid: true}"
+            :beatTitle="{value: 'New', valid: true}"
+            :beatBPM = "{value: 125, valid: true}"
+            :beatMp3Price = "{value: 25, valid: true}"
+            :beatWavPrice = "{value: 100, valid: true}"
+            :beatZipPrice = "{value: 250, valid: true}"
             :beatTags = "{value: [], valid: true}"
             :beatMode = "{value: 'upload'}"
         ></beat-upload>
@@ -36,7 +36,8 @@ export default {
   methods: {
     uploadBeat(formData) {
       this.isLoading = true;
-      this.$store.dispatch("beatStore/uploadBeat", formData);
+
+      this.$store.dispatch("beatStore/createOrUpdateBeat", formData);
       this.isLoading = false;
       this.$router.replace("/beats");
     },
